238c4cab006244288e5d7db8fc49fc67057b011a
Yasir <DPSA00609@mypaceschool.com>
2023-10-31 06:39:13 +0400

    Getting user data from html file.

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        email = request.form["email"]
+        date = request.form["doj"]
+        occupation = request.form["Occupation"]
     return render_template("index.html")
 
 if __name__ == "__main__":
